087699e7090be0f9029aed6e1a7f1f46c8d78297
TejasKulkarni2003 <104378810+TejasKulkarni2003@users.noreply.github.com>
2023-09-02 23:34:49 +0530

    Revert "updates"
    This reverts commit 2fc5bdec5d5f7e9568bed0d70fc20fb55698afa0.

--- contest.js
+++ contest.js
@@ -1,4 +1,5 @@
 let responsec = fetch("https://kontests.net/api/v1/code_chef")
+console.log(responsec)
 responsec.then((v)=>{
     return v.json()
 }).then((contest)=>{
@@ -27,30 +28,32 @@ responsecf.then((v)=>{
 function showcc(data){
     var latest = data.length-1;
     var localDate = new Date(data[latest].start_time)
-    // console.log(localDate)
+    console.log(localDate)
     var s = localDate + " ";
-    // console.log(s)
+    console.log(s)
     var day = s.slice(0, 3)
     var time = s.slice(16, 24)
     var conDate = s.slice(4, 16)
 
     setInterval(function() {
-        var now = new Date().getTime();
-        var time = localDate.getTime()
-        var distance = time - now;
 
+        // Get today's date and time
+        var now = new Date().getTime();
+      
+        // Find the distance between now and the count down date
+        var distance = localDate.getTime() - now;
+      
+        // Time calculations for days, hours, minutes and seconds
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
       
-        console.log(now);
-        console.log(time);
-        
+        // Display the result in the element with id="demo"
         document.getElementById("t1").innerHTML = days + "d " + hours + "h "
         + minutes + "m " + seconds + "s ";
       
-        
+        // If the count down is finished, write some text
         if (distance < 0) {
           clearInterval(x);
           document.getElementById("t1").innerHTML = "EXPIRED";
@@ -74,25 +77,32 @@ function showcc(data){
 function showlc(data){
     var latest = data.length-1;
     var localDate = new Date(data[latest].start_time)
-    // console.log(localDate)
+    console.log(localDate)
     var s = localDate + " ";
-    // console.log(s)
+    console.log(s)
     var day = s.slice(0, 3)
     var time = s.slice(16, 24)
     var conDate = s.slice(4, 16)
 
     setInterval(function() {
+
+        // Get today's date and time
         var now = new Date().getTime();
+      
+        // Find the distance between now and the count down date
         var distance = localDate.getTime() - now;
       
+        // Time calculations for days, hours, minutes and seconds
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
       
+        // Display the result in the element with id="demo"
         document.getElementById("t3").innerHTML = days + "d " + hours + "h "
         + minutes + "m " + seconds + "s ";
       
+        // If the count down is finished, write some text
         if (distance < 0) {
           clearInterval(x);
           document.getElementById("t3").innerHTML = "EXPIRED";
@@ -116,26 +126,32 @@ function showlc(data){
 function showcf(data){
     var latest = 0;
     var localDate = new Date(data[latest].start_time)
-    // console.log(localDate)
+    console.log(localDate)
     var s = localDate + " ";
-    // console.log(s)
+    console.log(s)
     var day = s.slice(0, 3)
     var time = s.slice(16, 24)
     var conDate = s.slice(4, 16)
 
     setInterval(function() {
+
+        // Get today's date and time
         var now = new Date().getTime();
+      
+        // Find the distance between now and the count down date
         var distance = localDate.getTime() - now;
       
+        // Time calculations for days, hours, minutes and seconds
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
       
+        // Display the result in the element with id="demo"
         document.getElementById("t2").innerHTML = days + "d " + hours + "h "
         + minutes + "m " + seconds + "s ";
       
-        
+        // If the count down is finished, write some text
         if (distance < 0) {
           clearInterval(x);
           document.getElementById("t2").innerHTML = "EXPIRED";
